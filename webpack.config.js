e5ca555ed9ccd7316cba45225269c8719dc1cb4a
Shokhabbos <urinovs999@gmail.com>
2024-01-23 01:52:34 +0500

    html and clean plugin title inject template

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,21 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-    mode:"development",
-   entry: {
-    main:'./src/index.js',
-    sub:"./src/strange.js"
-   },
-   output:{
-    filename:"[name].[contenthash].js",
-    path:path.join(__dirname,"dist")
- 
-},
-plugins:[
-    
-        new HtmlWebpackPlugin(),
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+    sub: "./src/strange.js",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.join(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Hello World in Webpack",
+      inject: "body",
+      template: "./src/index.html",
+    }),
     new CleanWebpackPlugin(), //
-    
-]
-}
+  ],
+};
